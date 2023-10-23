573b94f5b1025b0aa0aa4b592efc25708caa88a2
paraggandhiqa <paraggandhi.qa@gmail.com>
2023-10-23 16:18:57 +0530

    test commit

---
+++ src/test/java/com/api/parallelTests/ChromeTest.java
@@ -0,0 +1,16 @@
+package com.api.parallelTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ChromeTest {
+	
+	@Test
+	public void chromeTest() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/com/api/parallelTests/IncognitoTest.java
@@ -0,0 +1,19 @@
+package com.api.parallelTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class IncognitoTest {
+	
+	@Test
+	public void incognitoTest() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--incognito");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+}
